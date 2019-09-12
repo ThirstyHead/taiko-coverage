bffe23e8ac6cef5ac59f2dc91f7f63a18e4b00ff
Scott Davis <scott@thirstyhead.com>
2019-09-12 16:45:56 +0100

    entire dom

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const runCoverage = async () => {
   //console.log(await cdpClient.DOM.getDocument());
   //console.log(JSON.stringify(await cdpClient.DOM.getDocument(), null, 2));
   let doc = await cdpClient.DOM.getDocument({ depth: -1 });
-  console.log(doc);
+  console.log(JSON.stringify(doc, null, 2));
   //console.log(`doc.root.nodeId - ${doc.root.nodeId}`);
   //let rootHTML = await cdpClient.DOM.getOuterHTML({nodeId: doc.root.nodeId});
   //console.log(rootHTML);
