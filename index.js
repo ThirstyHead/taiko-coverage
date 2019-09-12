663a875141fc17a5fa7a78e00c683b3b1da2a481
Scott Davis <scott@thirstyhead.com>
2019-09-12 16:07:43 +0100

    root html

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const runCoverage = async () => {
   //console.log(await cdpClient.DOM.getDocument());
   //console.log(JSON.stringify(await cdpClient.DOM.getDocument(), null, 2));
   let root = await cdpClient.DOM.getDocument();
+  console.log(`root.nodeId - ${root.nodeId}`);
   let rootHTML = await cdpClient.DOM.getOuterHTML(root.nodeId);
   console.log(rootHTML, null, 2);
   descEvent.emit('success', 'runCoverage ran successfully');
