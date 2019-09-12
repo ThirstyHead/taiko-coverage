bd1279cd61887a897366ec2605d8afb36e34c832
Scott Davis <scott@thirstyhead.com>
2019-09-12 16:44:26 +0100

    getting entire dom tree

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ const runCoverage = async () => {
   //console.log(await cdpClient.Browser.getVersion());
   //console.log(await cdpClient.DOM.getDocument());
   //console.log(JSON.stringify(await cdpClient.DOM.getDocument(), null, 2));
-  let doc = await cdpClient.DOM.getDocument();
-  console.log(`doc.root.nodeId - ${doc.root.nodeId}`);
-  let rootHTML = await cdpClient.DOM.getOuterHTML({nodeId: doc.root.nodeId});
-  console.log(rootHTML);
+  let doc = await cdpClient.DOM.getDocument({ depth: -1 });
+  console.log(doc);
+  //console.log(`doc.root.nodeId - ${doc.root.nodeId}`);
+  //let rootHTML = await cdpClient.DOM.getOuterHTML({nodeId: doc.root.nodeId});
+  //console.log(rootHTML);
   descEvent.emit('success', 'runCoverage ran successfully');
 };
 
