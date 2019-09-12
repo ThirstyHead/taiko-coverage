4289ec3bd7e919b8a1e0d92615a55af407decbc2
Scott Davis <scott@thirstyhead.com>
2019-09-12 15:27:31 +0100

    getting handle to client

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let descEvent;
+let cdpClient;
 
 const init = (taiko, eventHandler, descEventHandler) => {
   descEvent = descEventHandler;
   eventHandler.on('createdSession', async (client) => {
-    console.log('taiko-coverage init');
+    cdpClient = client;
   });
 };
 
 const runCoverage = async () => {
   // console.log('taiko-coverage runCoverage');
   descEvent.emit('success', 'runCoverage ran successfully');
+  console.log(cdpClient);
 };
 
 module.exports = {
